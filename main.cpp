fe996cd1251b7e1a1b4523726f0e7f67d2ceafc5
zengbi <2818097988@qq.com>
2022-04-07 16:26:52 +0800

    add constructor

--- main.cpp
+++ main.cpp
@@ -19,6 +19,10 @@ template<size_t id, typename Head, typename ...args>
 struct tupleImpl<id, Head, args...>:
 	public tupleElement<id, Head>, tupleImpl<id + 1, args...> {
 	typedef Head typeValue; 
+	typedef tupleElement<id, Head> element;
+	typedef tupleImpl<id + 1, args...> next;
+	tupleImpl<id, Head, args...>(){}
+	tupleImpl<id, Head, args...>(const Head &x, const args &...xs):element(x), next(xs...) {}
 };
 
 template<size_t id,typename Head, typename ...args>
@@ -31,10 +35,10 @@ using Tuple = tupleImpl<0, args...>;
 
 int main() {
 	// Tuple<int,double,long long> x(1, 1.1234, 1LL<<55);
-	Tuple<int, float, std::string> tuple;
-    getVal<0>(tuple) = 5;
-    getVal<1>(tuple) = 8.3;
-    getVal<2>(tuple) = "Foo";
+	Tuple<int, float, std::string> tuple(5, 8.3, "Foo");
+    // getVal<0>(tuple) = 5;
+    // getVal<1>(tuple) = 8.3;
+    // getVal<2>(tuple) = "Foo";
     std::cout << getVal<0>(tuple) << std::endl;
     std::cout << getVal<1>(tuple) << std::endl;
     std::cout << getVal<2>(tuple) << std::endl;
